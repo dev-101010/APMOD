783ad94b7ab5b771c2c4e1a3786050548708bc21
dev-101010 <8311861+dev-101010@users.noreply.github.com>
2023-04-23 10:08:54 +0200

    Update load.js

--- APMOD/inject/load.js
+++ APMOD/inject/load.js
@@ -1,1 +1,4 @@
-
+window.addEventListener("load", APModPopup.load);
+window.addEventListener("load", APModDataSpy.load);
+window.addEventListener("load", APModFiller.load);
+window.addEventListener("load", APModSelector.load);

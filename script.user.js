eed0b6a1ffdd3cbdd818bb6f1e1174cb922bd25c
dev-101010 <8311861+dev-101010@users.noreply.github.com>
2024-04-05 06:02:25 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         APMod
 // @namespace    https://github.com/dev-101010/APMOD/
-// @version      1.2.3
+// @version      1.2.4
 // @description  APMod fügt Filter und Ausfüllhilfen in APM ein welche die Arbeit erleichtern.
 // @author       dev-101010
 // @match        https://eam.eu1.inforcloudsuite.com/*
+// @match        https://t.corp.amazon.com/*
 // @icon         https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/icon.png
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @resource     STYLE1 https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/style.css
 // @resource     STYLE2 https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/radialmenu.css
+// @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/copywo.js
 // @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/selector.js
 // @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/dataspy.js
 // @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/dataspyfilter.js
@@ -18,14 +20,15 @@
 // @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/radialmenu.js
 // ==/UserScript==
 
-/* globals APModPopup, APModDataSpy, APModFiller, APModSelector */
+/* globals APModPopup, APModDataSpy, APModFiller, APModSelector, APModCopyWo */
 
 GM_addStyle (GM_getResourceText("STYLE1"));
 GM_addStyle (GM_getResourceText("STYLE2"));
 
 window.addEventListener("load", ()=> {
-  if(APModPopup!=null)APModPopup.load();
-  if(APModDataSpy!=null)APModDataSpy.load()
-  if(APModFiller!=null)APModFiller.load()
-  if(APModSelector!=null)APModSelector.load()
+    if(APModPopup!=null)APModPopup.load();
+    if(APModDataSpy!=null)APModDataSpy.load();
+    if(APModFiller!=null)APModFiller.load();
+    if(APModSelector!=null)APModSelector.load();
+    if(APModCopyWo!=null)APModCopyWo.load();
 });

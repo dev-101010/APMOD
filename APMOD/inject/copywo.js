3524cc5dbebb70cd2e1d5b7f747e402f7af2b7f2
dev-101010 <8311861+dev-101010@users.noreply.github.com>
2025-01-11 12:26:17 +0100

    Update copywo.js

--- APMOD/inject/copywo.js
+++ APMOD/inject/copywo.js
@@ -22,26 +22,47 @@ APModCopyWo.load = () => {
             const woNumber = array.pop();
             e.innerHTML = e.innerHTML.replace(woNumber, "");
 
-            const link = e.appendChild(document.createElement("button"));
+	    const link = e.appendChild(document.createElement("button"));
             link.id = "APModCopyWoButton";
             link.textContent = woNumber;
             link.type="button";
-            link.addEventListener('click', function(e) {
-                APModCopyWo.copy(woNumber);
-            });
             link.style.cursor = 'pointer';
             link.style["background-color"] = 'transparent';
             link.style["color"] = 'inherit';
-              link.style["margin-block"] = 0;
-              link.style["margin-inline"] = 0;
-              link.style["padding-block"] = 0;
-              link.style["padding-inline"] = 0;
-              link.style["font-family"] = 'inherit';
-              link.style["font-size"] = '100%';
-              link.style["line-height"] = 1.15;
+            link.style["margin-block"] = 0;
+            link.style["margin-inline"] = 0;
+            link.style["padding-block"] = 0;
+            link.style["padding-inline"] = 0;
+            link.style["font-family"] = 'inherit';
+            link.style["font-size"] = '100%';
+            link.style["line-height"] = 1.15;
             link.style["border-block"] = 0;
             link.style["border-inline"] = 0;
             link.style["border-block-end"] = "1px dashed currentColor";
+            link.style.marginRight = '10px';
+            link.addEventListener('click', function(e) {
+                APModCopyWo.copy(woNumber);
+            });
+            const link2 = e.appendChild(document.createElement("button"));
+            link2.type="button";
+            link2.textContent = "➽";
+            link2.style.cursor = 'pointer';
+            link2.style["background-color"] = 'transparent';
+            link2.style["color"] = 'inherit';
+            link2.style["margin-block"] = 0;
+            link2.style["margin-inline"] = 0;
+            link2.style["padding-block"] = 0;
+            link2.style["padding-inline"] = 0;
+            link2.style["font-family"] = 'inherit';
+            link2.style["font-size"] = '100%';
+            link2.style["line-height"] = 1.15;
+            link2.style["border-block"] = 0;
+            link2.style["border-inline"] = 0;
+            link2.style.marginLeft = '10px';
+            link2.addEventListener("click", ()=>{
+                window.open("https://eu1.eam.hxgnsmartcloud.com/web/base/logindisp?tenant=AMAZONRMEEU_PRD&FROMEMAIL=YES&SYSTEM_FUNCTION_NAME=WSJOBS&USER_FUNCTION_NAME=WSJOBS&workordernum="+woNumber, '_blank');
+            });
+		  
           }
         }
       }

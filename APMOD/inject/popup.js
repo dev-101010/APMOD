42550571c7213c2af3e985897858064295cfa136
dev-101010 <8311861+dev-101010@users.noreply.github.com>
2025-02-28 17:38:13 +0100

    Update popup.js

--- APMOD/inject/popup.js
+++ APMOD/inject/popup.js
@@ -1,17 +1,23 @@
 const APModPopup = {timeOut:null};
 
 APModPopup.load = () => {
-	
 	if(APModPopup.Popup != null) APModPopup.Popup.remove();
 
 	APModPopup.Popup = document.createElement("div");
 	APModPopup.Popup.id = "fillerPopup";
 	APModPopup.Popup.classList.add("fillerPopup");
 
+    	APModPopup.Popup.headerC = document.createElement("div");
+    	APModPopup.Popup.headerC.style.borderBottom = "solid";
+    	APModPopup.Popup.headerC.style.borderBottomWidth = "1px";
+    	APModPopup.Popup.headerC.style.borderBottomColor = "black";
+    	APModPopup.Popup.headerC.style.marginBottom = "3px";
+    	APModPopup.Popup.appendChild(APModPopup.Popup.headerC);
+
 	APModPopup.Popup.header = document.createElement("label");
    	APModPopup.Popup.header.innerText = "APMOD";
    	APModPopup.Popup.header.style.color = "black";
-   	APModPopup.Popup.appendChild(APModPopup.Popup.header);
+   	APModPopup.Popup.headerC.appendChild(APModPopup.Popup.header);
 	
 	APModPopup.Popup.label = document.createElement("label");
 	APModPopup.Popup.label.style.color = "white";

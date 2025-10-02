f64d2daa9acae4c724941dcdd30155524a3ed930
dev-101010 <8311861+dev-101010@users.noreply.github.com>
2025-10-03 01:41:23 +0200

    Update options.js

--- APMOD/inject/options.js
+++ APMOD/inject/options.js
@@ -9,14 +9,7 @@ var APModOptions = (function () {
     TBclass.prototype.initComponent = function () {
       this.APModOptionsOrigInitComponent.apply(this, arguments);
       this.insert(this.items.length, {
-        xtype: "button",
-      // icon-only button (no text); uses Ext's built-in tool icon classes
-      iconCls: "x-tool x-tool-gear",
-      ui: "default-toolbar",
-      tooltip: "APMod",
-      focusable: false,
-      width: 28,   // tweak if your theme needs other sizing
-      height: 28,  // tweak if your theme needs other sizing
+      iconCls: "x-tool-tool-el x-tool-img x-tool-gear",
         menu: [
           // --- Header + separator ---
         { 

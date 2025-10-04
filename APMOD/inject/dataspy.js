09de84af6d88d639fd0a1d5bfaa360ef86d0e389
dev-101010 <8311861+dev-101010@users.noreply.github.com>
2025-10-04 07:01:14 +0200

    Update dataspy.js

--- APMOD/inject/dataspy.js
+++ APMOD/inject/dataspy.js
@@ -139,7 +139,6 @@ APModDataSpy.injectReadOnlyGrid = () => {
 			}
 
 			if (this.gridURL.includes("EWSUSR.TAB")) {
-				console.log(this);
 				const list = this.getDockedItems('toolbar[dock="bottom"]');
 				if(list.length > 0) {
 					const botToolbar = this.getDockedItems('toolbar[dock="bottom"]')[0];
@@ -1638,6 +1637,7 @@ APModDataSpy.filterValues = [
 ];
 
 //window.addEventListener("load", APModDataSpy.load);
+
 
 
 
